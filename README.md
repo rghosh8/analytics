511c02e656b10bc6cc19032dcb558bdc8d47dfc0
rghosh8 <rajat.ghosh11@gmail.com>
2019-03-09 02:49:50 -0800

    Added dynamic threshold and README

--- README.md
+++ README.md
@@ -1,1 +1,119 @@
-# analytics
+# stat
+
+#### This document aims to explain basicParameters.js, which includes basic statistical utilities
+
+#### First few basic statistical parameters are computed:
+
+* Size of a time series
+* Sum of all the elements in the series
+* Mean of the series
+* Simple moving average
+* n-th order moment
+* Variance
+* Standard Deviation
+* Skewness
+* Kurtosis
+
+## Anomaly Detection (**isAnomaly(present, data, num)**)
+
+#### Return if the sensor is anomalous
+  
+  * Parameters
+    * present
+      * It represents current sensor value
+    * data
+      * Data represent time series data
+    * num
+      * num respresnt the time window
+  * Return
+    * true/false: a boolean representing if the sensor is anomalous
+
+#### Comment
+
+* Case 1: Standard deviation is zero, but Mean is not equal to the present data
+  * A sudden change in the sensor value. 
+
+* Case 2: Skewness < 0 && Kurtosis > 0
+  * It means the data has a long left tail, possible liklihood of cybersecurity issues
+  * Test
+    * present < Mean(data) - factor * Std(data)
+        * factor = 1.5
+        * We have to be little defensive here
+      
+* Case 3: Kurtosis > 0
+  * It means the data has long tail, increasing liklihood of anomaly
+  * Test
+    * (present > Mean(data) + factor * Std(data)) || (present < Mean(data) - factor * Std(data))
+      * factor = 2
+
+* Default
+  * Test
+    * present > (Mean(data) + factor * Std(data)) || (present < Mean(data) - factor * Std(data))
+      * factor = 3 (three sigma)
+
+  
+## Volatility Risk (**VI(data, num)**) Assessment
+
+#### Return the volatility risk of the sensor
+
+* Parameters
+    * data
+      * It represents rolling window data
+    * num
+      * It represents rolling window size
+  * Return
+    * It represents a measure of volatility for the sensor
+
+#### Comment
+* Our volatility measure is coefficient of variance
+  * Ratio of standard deviation and mean in rolling window
+
+## Compliance Risk (**VI(present, upper, lower)**) Assessment
+
+#### Return the compliance risk of the sensor
+
+* Parameters
+    * present
+      * It represents the current sensor value
+    * data
+      * It represents rolling window data
+    * num
+      * It represents rolling window size
+  * Return
+    * It represents a measure of complaince for the sensor
+
+#### Comment
+* Our compliance risk is measured by min-max noramlization
+  * CI = (present - lower)/(upper - lower)
+    * upper and lower thresholds are not hard coded, rather they are computed on a data-driven rolling window.
+
+## Overall Sensor Health (**SensorHealthScore(CI, VI, thresholds, alpha)**) Assessment
+
+#### Return the score that commbines overall sensor health, including compliance and volatility risks
+
+* Parameters
+    * CI
+      * It represents the compliance risk
+    * VI
+      * It represents the volatility risk
+  
+    * alpha
+      * Relative importance of compliance risk over volatility risk
+    * data
+      * It represents rolling window data
+    * num
+      * It represents rolling window size
+
+  * Return
+    * It represents a health score for the device
+
+#### Comment
+* Twelve different scenarios are considered to determine the sensor health score
+  * Case 1: Both CI and VI are defined
+    * Score: null
+    
+
+
+
+ 
+
--- basicParameters.js
+++ basicParameters.js
@@ -1,66 +1,48 @@
 
 let inputData = [1, 2, 3, 9, 11, 12, 15, 17, 18, 21, 23, 24, 27];
 
-//
+// This suporting function takes a time series data and find total number of elements 
 const Len = (data) => data.length
 console.log(Len(inputData))
 
-// sum
+// This supporting function takes a time series data and find the sum of the elements 
 const Sum = (data) => data.reduce((acc, curr) => acc + curr);
 console.log(Sum(inputData))
 
-// mean
+// This supporting function takes a time series data and find the Mean of the elements
 const Mean = (data) => Sum(data)/Len(data)
 console.log(Mean(inputData))   
 
-// Moving Average (choosing )
+// This supporting function takes a time series data and find the simple moving average. num is 
+// the number of previous data points used for the moving average computation. 
 
-const MovingAverage = (data, num) => Sum(data.slice(-num))/Len(data.slice(-num))
-console.log(MovingAverage(inputData, num=2))
+const SMA = (data, num) => Sum(data.slice(-num))/Len(data.slice(-num))
+console.log(SMA(inputData, num=2))
 
-//mean substraction Nth order
+// Statistical moment is an important parameter for understanding the shape of the distribution
 
-const  MeanSubN = (data, n) => data.map(element => Math.pow((element - Mean(data)), n))
-console.log(MeanSubN(inputData, 2))
-console.log(MeanSubN(inputData, 3))
+const  nStatMoment = (data, n) => data.map(element => Math.pow((element - Mean(data)), n)).reduce((acc, curr) => acc + curr);
+console.log(nStatMoment(inputData, 2))
+console.log(nStatMoment(inputData, 3))
 
+// Variance (bias corrected)
 
-
-// sum of mean substraction square list
-
-const MeanSubSqSum = (data) => MeanSubN(data, 2).reduce((acc, curr) => acc + curr)
-console.log(MeanSubSqSum(inputData))
-
-
-//variance (bias corrected)
-
-const Variance = (data) => MeanSubSqSum(data)/(Len(data) - 1)
+const Variance = (data) => nStatMoment(data, 2)/(Len(data) - 1)
 console.log(Variance(inputData))
 
-//standard deviation
-
 const Std = (data) => Math.sqrt(Variance(data))
-console.log(Std(inputData))
-
-// sum of mean substraction cube list
-
-const MeanSubCubeSum = (data) => MeanSubN(data, 3).reduce((acc, curr) => acc + curr)
-console.log(MeanSubCubeSum(inputData))
+console.log('standard deviation --- ', Std(inputData))
 
 //Skewness
-const Skew = (data) => MeanSubCubeSum(data)/Math.pow(Variance(inputData), 1.5)/Len(data)
-console.log(Skew(inputData))
-
-// sum of mean substraction 4th order list
-const MeanSub4Sum = (data) => MeanSubN(data, 4).reduce((acc, curr) => acc + curr)
-console.log(MeanSub4Sum(inputData))
+const Skew = (data) => nStatMoment(data, 3)/Math.pow(Std(inputData), 3)/Len(data)
+console.log('skewness --- ', Skew(inputData))
 
 //Excess Kurtosis
-const Kurt = (data) => (MeanSub4Sum(data)/Math.pow(Variance(data), 2)/Len(data)) - 3
+const Kurt = (data) => nStatMoment(data, 4)/Math.pow(Std(data), 4)/Len(data) - 3
 console.log('Excess Kurtosis ---', Kurt(inputData))
 
-//Anomaly detection
-
+ //Anomaly detection
+ // The entire horizon is computed on a rolling basis
 const isAnomaly = (present, data, num) => {
   data = data.slice(-num)
   if(Std(data) === 0 && Mean(data) !== present){
@@ -77,76 +59,80 @@ const isAnomaly = (present, data, num) => {
   let factor = 3;
   return present > (Mean(data) + factor * Std(data)) || (present < Mean(data) - factor * Std(data))
 }
+console.log('isAnomaly ---', isAnomaly(111, inputData, 10))
 
-console.log('isAnomaly ---', isAnomaly(111, inputData, 2))
-
-// Volatility Index
-
+// Volatility Risk defined as the coefficient varaince
 const VI = (data, num) => {
   data = data.slice(-num)
   return Std(data)/Mean(data)
 }
-console.log('VI---', VI(inputData, 2))
+console.log('VI---', VI(inputData, 10))
 
-// Compliance Index
-
-const CI = (present, upper, lower) => (present - lower)/(upper - lower)
-console.log('CI ---- ', CI)
-
-
-//Very basic forecasting
-const PointForecast = (data, num) => MovingAverage(data, num)
-console.log('PointForecast ------', PointForecast(inputData, 3));
-
-let result = []
-const HorizonForecast = (data, num, horizon, factor) => {
-  if (horizon > factor*num){
-    console.log('Critical Horizon ---', factor*num)
-    return -1
-  }
-  if (horizon < 1) {
-    return -1
-  }
-  if (horizon === 1){
-     result.push(PointForecast(data, num))
-    
-  } else {
-    result.push(HorizonForecast(data, num, horizon - 1, factor));
-  }
-  return result
+//Compliance Index
+const CI = (present, data, num) => {
+  data = data.slice(-num);
+  let factor = 3;
+  let upper = Mean(data) + factor * Std(data);
+  let lower = Mean(data) - factor * Std(data);
+  return (present - lower)/(upper - lower)
 }
-/// fix the recursion problem
-console.log('HorizonForecast ------', HorizonForecast(inputData, 10, 3, 0.5));
+console.log('CI ---- ', CI(15, inputData, 10))
+
+// Dynamic thresholds
+const Thresholds = (data, num) => {
+  data = data.slice(-num); 
+  let factorCI;
+  let factorVI;
+  let warningToAlarm = 0.9;
+
+  Kurt(data) < 0 ?  factorCI = 3: factorCI = 2;
+  let marginCI = {
+    lowWarning: Mean(data) - (factorCI * warningToAlarm) * Std(data),
+    lowAlarm: Mean(data) - (factorCI) * Std(data),
+    highWarning: Mean(data) + (factorCI * warningToAlarm) * Std(data),
+    highAlarm: Mean(data) + (factorCI) * Std(data)
+  }
+  Kurt(data) < 0 ?  factorVI = 1: factorVI = 0.75;
+  let marginVI = {
+    warning: 0.1 * factorVI,
+    alarm: 0.2 * factorVI
+  } 
+  return [marginCI, marginVI];
+}
 
 // Finding a real-time health score
 //alpha determines relative weight of compliance risk over the volatility risk
-const SensorHealthScore = (CI, VI, thresholds, alpha) => {
-  if (alpha < 0 || alpha >1) return {score: -1, message: 'alpha must be between 0 (inclusive) and 1 (inclusive'};
-  if (CI === undefined && VI === undefined) return {score: -1, message: 'undefined'};
+const SensorHealthScore = (CI, VI, alpha, data, num) => {
+  let [marginCI, marginVI] = Thresholds(data, num);
+  if (alpha < 0 || alpha >1) return {score: null, message: 'alpha must be between 0 (inclusive) and 1 (inclusive'};
+
+  if (CI === undefined && VI === undefined) return {score: null, message: 'undefined'};
   if (VI === undefined) return {score: CI, message: 'volatility risk is not defined'};
   if (CI === undefined) return {score: VI, message: 'compliance risk is not defined'};
-  if (CI < thresholds.lowCI) return {score: 0, message: 'lower threshold breached'};
-  if (CI > thresholds.highCI) return {score: 0, message: 'upper threshold breached'};
-  if (VI > thresholds.highVI)  return {score: 0, message: 'volatility margin breached'};
-  if (CI < thresholds.lowMidCI && VI > thresholds.highMidVI)  return {score: 0, message: 'high volatility near lower threshold'}; 
-  if (CI > thresholds.highMidCI && VI > thresholds.highMidVI) return {score: 0, message: 'high volatility near upper threshold'};
-  if (CI < thresholds.lowMidCI)  return {score: (CI - thresholds.lowMidCI)/(thresholds.lowCI - thresholds.lowMidCI), message: 'operating near lower threshold'};
-  if (CI > thresholds.highMidCI) return {score: (thresholds.highCI - CI)/(thresholds.highCI - thresholds.highMidCI), message: 'operating near upper threshold'};
-  if (VI > thresholds.highMidVI)  return {score: (thresholds.highVI- VI)/(thresholds.highVI - thresholds.highMidVI), message: 'operating with high volatility'};
-  let complianceRisk = (CI - thresholds.lowMidCI)/(thresholds.highMidCI - thresholds.lowMidCI);
-  let volatilityRisk = (thresholds.highVI - VI)/(thresholds.highVI - thresholds.highMidVI)
+
+  if (CI < marginCI.lowAlarm) return {score: 0, message: 'lower threshold breached'};
+  if (CI > marginCI.higAlarm) return {score: 0, message: 'upper threshold breached'};
+  if (VI > marginVI.alarm)  return {score: 0, message: 'volatility margin breached'};
+
+  if (CI < marginCI.lowWarning && VI > marginVI.warning)  return {score: 0, message: 'high volatility near lower threshold'}; 
+  if (CI < marginCI.lowWarning)  return {score: (CI - marginCI.lowAlarm)/(marginCI.lowWarning - marginCI.lowAlarm), message: 'operating near lower threshold'};
+  
+  if (CI > marginCI.highAlarm && VI > marginVI.warning) return {score: 0, message: 'high volatility near upper threshold'};
+  if (CI > marginCI.highWarning) return {score: (marginCI.highAlarm - CI)/(marginCI.highAlarm - marginCI.highWarning), message: 'operating near upper threshold'};
+  
+  if (VI > marginVI.warning)  return {score: (marginVI.alarm- VI)/(marginVI.alarm - marginVI.warning), message: 'operating with high volatility'};
+  
+  let complianceRisk = (CI - marginCI.lowWarning)/(marginCI.highWarning - marginCI.lowWarning);
+  let volatilityRisk = (marginVI.alarm - VI)/(marginVI.alarm - marginVI.warning)
   let testScore = alpha * complianceRisk  + (1 - alpha) * complianceRisk;
   return {score: testScore, message: 'healthy sensor' };
 }
-console.log(HealthScore(0.25, 0.02, sampleThresholds, 0.5))
-let sampleThresholds= {
-   lowCI: 0.01,
-   lowMidCI: 0.1,
-   highMidCI: 0.9,
-   highCI: 0.99,
-   highVI: 0.2,
-   highMidVI: 0.1
-}
 
 
-const DeviceHealthScore = (CollectionSensorHealthScore, num) => MovingAverage(CollectionSensorHealthScore,num)
+
+console.log(SensorHealthScore(0.25, 0.02, 0.5, inputData, 10))
+
+const DeviceHealthScore = (CollectionSensorHealthScore, num) => SMA(CollectionSensorHealthScore,num)
+
+
+
