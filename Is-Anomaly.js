2705ea9255e905ae1eec028b69e568661cb57bcd
rghosh8 <rajat.ghosh11@gmail.com>
2019-03-10 22:40:22 -0700

    Added Message to Anomaly

--- Is-Anomaly.js
+++ Is-Anomaly.js
@@ -2,16 +2,19 @@ import * as stat from "./stat.js";
 const isAnomaly = (present, data, num) => {
   data = data.slice(-num)
   if(stat.Std(data) === 0 && stat.Mean(data) !== present){
-    return true
+    return {isAnomaly: true, message: 'Component Error'}
   }
   if(stat.Skew(data) < 0 && stat.Kurt(data) > 0){
-    let factor = 1.5
-    return present < stat.Mean(data) - factor * stat.Std(data);
+    let factor = 1.5;
+    let flag = present < stat.Mean(data) - factor * stat.Std(data);
+    return {isAnomaly: flag, message: 'Low Temperature Risk'};
   }
   if(stat.Kurt(data) > 0){
     let factor = 2
-    return (present > stat.Mean(data) + factor * stat.Std(data)) || (present < stat.Mean(data) - factor * stat.Std(data))
+    let flag = (present > stat.Mean(data) + factor * stat.Std(data)) || (present < stat.Mean(data) - factor * stat.Std(data));
+    return {isAnomaly: flag, message: 'Bursty Workload'};
   }
   let factor = 3;
-  return present > (stat.Mean(data) + factor * stat.Std(data)) || (present < stat.Mean(data) - factor * stat.Std(data))
+  let flag = present > (stat.Mean(data) + factor * stat.Std(data)) || (present < stat.Mean(data) - factor * stat.Std(data));
+  return {isAnomaly: flag, message: 'Abnormal Operational Characteristics'};
 }
